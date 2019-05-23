062fe33f874a37b64f0eb3f2818ba18e7cc9047c
Liberbell <liberbell@gmail.com>
2019-05-24 07:38:09 +0900

    urls3

--- nandiasgarden/nandiasgarden/urls.py
+++ nandiasgarden/nandiasgarden/urls.py
@@ -19,5 +19,7 @@ from pizza import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.home, name='home')
+    path('', views.home, name='home'),
+    path('order', views.order, name='order'),
+
 ]
