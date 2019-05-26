06060c8e2cc43d4424ec0324c47bec32269c1155
Liberbell <liberbell@gmail.com>
2019-05-26 15:17:43 +0900

    forms4

--- nandiasgarden/pizza/forms.py
+++ nandiasgarden/pizza/forms.py
@@ -3,3 +3,4 @@ from django import forms
 class PizzaForm(forms.Form):
     topping1 = forms.CharField(label='Topping 1', max_length=100)
     topping2 = forms.CharField(label='Topping 2', max_length=100)
+    seze = forms.ChoiseField(label='Size', choises=[('small', 'Small'), ('medium', 'Medium'), ('large', 'Large')])
