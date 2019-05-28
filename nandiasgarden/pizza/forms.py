ae8490752a7f9b1f759439f6f3bfdd2409ec2832
Liberbell <liberbell@gmail.com>
2019-05-29 07:36:06 +0900

    forms2

--- nandiasgarden/pizza/forms.py
+++ nandiasgarden/pizza/forms.py
@@ -10,4 +10,4 @@ class PizzaForm(forms.ModelForm):
     class Meta:
         model = Pizza
         fields = ['topping1', 'topping2', 'size']
-        labels = {'topping1':'Topping 1'}
+        labels = {'topping1':'Topping 1', 'topping2':'Topping 2'}
