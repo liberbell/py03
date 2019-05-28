9026fcad116d3ab57e2130d46e6bbad29ea97968
Liberbell <liberbell@gmail.com>
2019-05-29 07:49:16 +0900

    forms8

--- nandiasgarden/pizza/forms.py
+++ nandiasgarden/pizza/forms.py
@@ -2,9 +2,9 @@ from django import forms
 from .models import Pizza
 
 class PizzaForm(forms.Form):
-    # topping1 = forms.CharField(label='Topping 1', max_length=100, widget=forms.PasswordInput)
-    # topping2 = forms.CharField(label='Topping 2', max_length=100)
-    toppings = forms.MultipleChoiceField(choices=[('pep','Pepperoni'),('cheese','Cheese'),('olives','Olives'),('onion','Onion')], widget=forms.CheckboxSelectMultiple)
+    topping1 = forms.CharField(label='Topping 1', max_length=100)
+    topping2 = forms.CharField(label='Topping 2', max_length=100)
+    # toppings = forms.MultipleChoiceField(choices=[('pep','Pepperoni'),('cheese','Cheese'),('olives','Olives'),('onion','Onion')], widget=forms.CheckboxSelectMultiple)
     size = forms.ChoiceField(label='Size', choices=[('Small', 'Small'), ('Medium', 'Medium'), ('Large', 'Large'), ('Extra Large', 'Extra Large')])
 
 # class PizzaForm(forms.ModelForm):
