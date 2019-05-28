9e3c852e73dda5eb80b6b82dc05d02cd8d2cfc66
Liberbell <liberbell@gmail.com>
2019-05-29 07:54:01 +0900

    forms11

--- nandiasgarden/pizza/forms.py
+++ nandiasgarden/pizza/forms.py
@@ -8,8 +8,10 @@ from .models import Pizza
 #     size = forms.ChoiceField(label='Size', choices=[('Small', 'Small'), ('Medium', 'Medium'), ('Large', 'Large'), ('Extra Large', 'Extra Large')])
 
 class PizzaForm(forms.ModelForm):
+    size = forms.ModelChoiceField(queryset=Size.objects, empty_label=None, widget=forms.CheckboxSelectMultiple)
+
     class Meta:
         model = Pizza
         fields = ['topping1', 'topping2', 'size']
         labels = {'topping1':'Topping 1', 'topping2':'Topping 2'}
-        widgets = {'size':forms.CheckboxSelectMultiple}
+        # widgets = {'size':forms.CheckboxSelectMultiple}
