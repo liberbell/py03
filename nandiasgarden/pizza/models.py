c4b29f678b59d83289581026c236f6adb10fe4de
Liberbell <liberbell@gmail.com>
2019-05-28 12:08:05 +0900

    models3

--- nandiasgarden/pizza/models.py
+++ nandiasgarden/pizza/models.py
@@ -10,3 +10,4 @@ class Size(models.Model):
 class Pizza(models.Model):
     topping1 = models.CharField(max_length=100)
     topping2 = models.CharField(max_length=100)
+    size = models.ForeignKey(Size, on_delete=models.CASCADE)
