29d75d7138da4a4558d977e93313d934dd524362
Liberbell <liberbell@gmail.com>
2019-06-01 09:03:40 +0900

    urls1

--- nandiasgarden/nandiasgarden/urls.py
+++ nandiasgarden/nandiasgarden/urls.py
@@ -21,6 +21,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home, name='home'),
     path('order', views.order, name='order'),
-    path('pizzas' views.pizzas, name='pizzas'),
+    path('pizzas', views.pizzas, name='pizzas'),
 
 ]
