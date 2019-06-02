823ce2546d27109d7e786015cdfb7b50d2598fc0
Liberbell <liberbell@gmail.com>
2019-06-02 11:18:17 +0900

    forms1

--- nandiasgarden/pizza/forms.py
+++ nandiasgarden/pizza/forms.py
@@ -10,6 +10,7 @@ from .models import Pizza, Size
 class PizzaForm(forms.ModelForm):
     # size = forms.ModelChoiceField(queryset=Size.objects, empty_label=None, widget=forms.RadioSelect)
     # image = forms.ImageField()
+    email = forms.EmailField()
 
     class Meta:
         model = Pizza
